0b97a1a6934dcaea4d82a437814319e14688288e
Hairaa-1026 <1020071381@qq.com>
2021-01-04 22:15:55 +0800

    update sentiment analysis

--- src/sentiment_analysis/fill_emotion.py
+++ src/sentiment_analysis/fill_emotion.py
@@ -1,18 +1,19 @@
 from pathlib import Path
-
 import pandas as pd
 from snownlp import SnowNLP
-
+from senta import Senta
 
 
 def fill_emotion():
+    my_senta = Senta()
+    my_senta.init_model(model_class="ernie_1.0_skep_large_ch", task="sentiment_classify", use_cuda=False)
     comments_csv_path = Path('.') / '../data/comments.csv'
     doc = pd.read_csv(comments_csv_path)
     doc = doc.copy()
     row_num, col_num = doc.shape
     for i in range(row_num):
         text = doc['content'].loc[i]
-        doc['emotion'].loc[i] = SnowNLP(text).sentiments
+        doc['emotion'].loc[i] = my_senta.predict(text)[0][1]
 
     doc.to_csv(comments_csv_path, index=False, encoding='utf-8')
 
